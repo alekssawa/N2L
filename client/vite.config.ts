063403d4ca62340eadbd792dbc3e0e4727419d7c
babykiwii <aleks20yara@gmail.com>
2025-07-31 18:43:55 +0300

    update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/N2L/client/',
+  base: '/N2L/',
   plugins: [react(), tailwindcss()],
 });
